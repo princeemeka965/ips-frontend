cbe2fa7b80944fb70b7b52619ae02bd4b1db3e5e
princeemeka965 <anyanwue4@gmail.com>
2023-03-24 01:07:15 +0100

    built header, first segment section

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,32 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import BadgeComponent from './components/BadgeComponent.vue'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="w-full h-full flex flex-col lg:py-10 lg:px-20 sm:py-10 sm:px-20 py-10 px-5">
+    <!--- Set Logo as Header -->
+    <div class="lg:flex md:flex sm:flex hidden">
+      <img src="./assets/icons/ips-logo.svg" />
+    </div>
+    <!--- Set Section of Web Page --->
+    <div
+      class="w-full flex flex-col lg:flex lg:flex-row md:flex md:flex-row lg:my-16 sm:my-20 my-5"
+    >
+    <!-- First Segment of Section featuring Email Input for notification ---->
+      <div class="flex lg:w-1/2 w-full justify-start lg:py-10">
+        <div class="flex flex-col flex-grow lg:my-10">
+          <div class="w-4/5">
+            <badge-component title="New Course" />
+            <div class="lg:py-3 py-4">
+              <span class="lg:text-6xl md:text-6xl sm:text-4xl text-4xl"> iPhone Photo Academy </span>
+            </div>
+            <div class="lg:py-2"><span class="lg:text-xl">Sign up now to get notified when this course is available!</span></div>
+          </div>
+        </div>
+      </div>
 
     </div>
-  </header>
-
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/BadgeComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+<div class="py-2 px-4 rounded-full badge-bg max-w-max">
+<span class="uppercase text-xs">
+{{title}}
+</span>
+</div>
+</template>
+
+<style scoped>
+.badge-bg {
+  background-color: #2B5341;
+}
+</style>
